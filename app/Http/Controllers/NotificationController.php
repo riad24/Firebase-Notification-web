4efbe6b9812f5971051daf8619222bf6950c2523
Riad Hossain <riadmahmudeng@gmail.com>
2020-05-12 01:37:10 +0600

    Firebase Notification

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\PostNotifaction;
+
+class NotificationController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required|max:255',
+            'description' => 'required',
+        ]);
+
+        $input = array_except($request->all(),array('_token'));
+         $post = new PostNotifaction;
+         $post->AddData($input);
+
+
+
+        \Session::put('success','Post send notification successfully!!');
+
+        return redirect()->route('post.index');
+    }
+
+    public function notification($token, $title)
+    {
+        $fcmUrl = 'https://fcm.googleapis.com/fcm/send';
+        $token=$token;
+
+        $notification = [
+            'title' => $title,
+            'sound' => true,
+        ];
+
+        $extraNotificationData = ["message" => $notification,"moredata" =>'dd'];
+
+        $fcmNotification = [
+            //'registration_ids' => $tokenList, //multple token array
+            'to'        => $token, //single token
+            'notification' => $notification,
+            'data' => $extraNotificationData
+        ];
+
+        $headers = [
+            'Authorization: key=Legacy server key',
+            'Content-Type: application/json'
+        ];
+
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$fcmUrl);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fcmNotification));
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return true;
+    }
+}
